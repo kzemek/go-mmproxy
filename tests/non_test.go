a26798bf0e7706db931035fb60c6be21f51e5874
Konrad Zemek <konrad.zemek@gmail.com>
2025-07-28 01:00:02 +0200

    Add copyright notice to the new file

--- tests/non_test.go
+++ tests/non_test.go
@@ -1,3 +1,7 @@
+// Copyright 2025 Konrad Zemek <konrad.zemek@gmail.com>
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package tests
 
 import (
